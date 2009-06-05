77e2fd104fcc827848e693b6badb20af87a3203c
nstehr <nstehr@c886e5ce-1809-11de-b413-eb0469710719>
2009-06-05 00:36:45 +0000

    added some better error handling, and upgraded transmissionClient api handler to work with the latest version of transmission

--- trunk/python-transmission/python-transmission/transmissionClient/transmissionClient.py
+++ trunk/python-transmission/python-transmission/transmissionClient/transmissionClient.py
@@ -16,6 +16,7 @@ transmissionClient.py
 from simplejson import dumps, loads
 from urllib import urlopen
 import sys
+import httplib
 
 
 class TransmissionClientFailure(Exception): pass
@@ -23,30 +24,47 @@ class TransmissionClientFailure(Exception): pass
 class TransmissionClient(object):
   
   rpcUrl = None
-
+  sessionId = None
   def __init__( self, rpcUrl='http://localhost:9091/transmission/rpc' ):
     """ try to do a stupid call to transmission via rpc """
 
     try:
       self.rpcUrl = rpcUrl
-      #[Nathan Stehr March 7, 2009] comment this out, in transmission 1.5 connecting to the base rpc url just hangs
-      #urlopen( rpcUrl ).read()
+      #Nathan Stehr: June 4, 2009: 
+      #the new version of transmission uses a session key, so make a dummy
+      #request and get the session id from the headers 
+      self._getSessionID()
 
     except Exception, e:
       raise TransmissionClientFailure, \
             "Make sure your transmission-daemon is running  %s" % e
 
 
-
+  def _getSessionID(self):
+       conn = httplib.HTTPConnection("localhost:9091")
+       conn.request("GET", "/transmission/rpc?")
+       response = conn.getresponse()
+       self.sessionId = response.getheader('x-transmission-session-id')
+  
   def _rpc( self, method, params=[] ):
     """ generic rpc call to transmission """
 
     postdata = dumps({ 'method': method, 
                        'arguments': params, 
-                       'id': 'jsonrpc'})
-
+                       })
+    
     try:
-      response = loads( urlopen( self.rpcUrl , postdata).read() )
+        conn = httplib.HTTPConnection("localhost:9091")
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain","x-transmission-session-id":self.sessionId}
+        conn.request("POST", "/transmission/rpc", postdata,headers)
+        response = conn.getresponse()
+        status = response.status
+        if status==409:
+            self._getSessionID()
+            self._rpc(method,params)
+        data = response.read()
+        conn.close()
+        return loads(data)
     except Exception, e:
       raise TransmissionClientFailure, \
             "Make sure your transmission-daemon is running  %s" % e
@@ -60,7 +78,10 @@ class TransmissionClient(object):
 
 
   def torrentGet( self, torrentIds='', fields=[ 'id', 'name']):
-    return self._rpc( 'torrent-get', { 'ids': torrentIds, 'fields': fields } ) 
+    if torrentIds == '':
+        return self._rpc( 'torrent-get', {'fields': fields } )
+    else:
+        return self._rpc( 'torrent-get', { 'ids': torrentIds, 'fields': fields } ) 
 
   def torrentAdd( self, torrentFile, downloadDir='.' ):
     return self._rpc( 'torrent-add', { 'filename': torrentFile, 
